184fcd3e2730224293100ca536f4b21ff86f4d4f
hopewu0320 <gccbs01596@gmail.com>
2023-02-06 11:47:11 +0800

    amount3

--- base/models.py
+++ base/models.py
@@ -5,12 +5,12 @@ class Drink(models.Model):
     #user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True,blank=True)
-    price = models.IntegerField()
+    price = models.IntegerField(null=True)
     creadted = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     image = models.CharField(max_length=40, null=True,blank=False)
 
 class Cart(models.Model):
     drink = models.ForeignKey(Drink,on_delete=models.SET_NULL,null=True)
-    total = models.IntegerField()
-
+    amount = models.CharField(max_length=200,null=True)
+    
--- base/views.py
+++ base/views.py
@@ -13,23 +13,31 @@ def productinfo(request,pk):
     context = {'drink':drink}
     return render(request, 'base/productinfo.html',context)
 
-#需要物品的數量 價錢
+#按下購物車按鈕 商品的amount數量上升
+#如果drink.name not exist,create object else 加入
+#https://stackoverflow.com/questions/11714536/check-if-an-object-exists
+#用filter 可以數物品數
 def addtocart(request,pk):    #東西加入完購物車 導向cart
     drink = Drink.objects.get(id=pk)
-    total = drink.price
     if request.method == "POST":
-        Cart.objects.create(
-        drink = drink,
-        total = total
-        )
-        context = {'total':total}
+        if Cart.objects.filter(drink=drink).exists():
+            cart = Cart.objects.get(drink=drink)
+            cart.amount = cart.amount+1
+            cart.save()
+        else:
+            Cart.objects.create(
+            drink = drink,
+            name = drink.name,
+            amount=1,
+            )  
         return redirect('cart')
     #context = {'total':total}
     #return render(request, 'base/addtocart.html',context)
 def cart(request):
-    carts =Cart.objects.all()
-    total = 0
+    carts = Cart.objects.all()
+    total = 0 
     for cart in carts:
         total+=cart.drink.price
     context = {'carts':carts,'total':total}
     return render(request, 'base/cart.html',context)
+
