242d4d39a7b19808a63341292ae65109e072338c
hopewu0320 <gccbs01596@gmail.com>
2023-02-13 21:34:24 +0800

    customer

--- base/models.py
+++ base/models.py
@@ -12,9 +12,18 @@ class Drink(models.Model):
     def __str__(self):
         return self.name
 
+#Order 裡面要有Cart跟customer 每個Order搭配一個Cart, Cart裡面不需要有user
+#customer要有user phone address
+class Customer(models.Model):
+    user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    phone = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
 class Cart(models.Model):
     drink = models.ForeignKey(Drink,on_delete=models.SET_NULL,null=True)
-    user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True)
     amount = models.IntegerField(null=True)
     subtotal = models.IntegerField(null=True)
     def __str__(self):
@@ -24,5 +33,3 @@ class Cart(models.Model):
             self.amount = 1
         self.subtotal = self.drink.price * self.amount
         super(Cart, self).save(*args, **kwargs)
-#Order 裡面要有Cart跟customer 每個Order搭配一個Cart, Cart裡面不需要有user
-#customer要有user phone address
